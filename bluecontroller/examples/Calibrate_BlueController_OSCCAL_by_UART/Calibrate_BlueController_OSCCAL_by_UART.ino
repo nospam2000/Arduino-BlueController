142e73a0ea8c2cbf86ce519009b704a75278a338
Michael Dreher <Michael.Dreher42@gmail.com>
2013-05-25 13:05:13 +0200

    Cleaned up the code for better readability

--- bluecontroller/examples/Calibrate_BlueController_OSCCAL_by_UART/Calibrate_BlueController_OSCCAL_by_UART.ino
+++ bluecontroller/examples/Calibrate_BlueController_OSCCAL_by_UART/Calibrate_BlueController_OSCCAL_by_UART.ino
@@ -1,29 +1,37 @@
 /*
- Calibrate the OSCCAL value on the UART of the Bluetooth controller without using additional wiring (ICP1 would have been helpful)
+ Calibrate the OSCCAL value to match the baud rate of the communication partner without using additional wiring (ICP1 would have been helpful)
+ or hardware (logic analyzer or oscilloscope).
+ 
+ Why you might need this? Using an ATmega MCU using the internal RC oscillator ()
+ 
  This compensates the following timing errors:
  1: the baud rate error (which is -3.5% at 115200 baud)
- 2: the oscillator speed
+ 2: the factory calibration of the oscillator speed
  
- The oscillator speed adapts to the serial speed of the communication partner, so that the ATmega matches the baud rate
- of its partner. Only tolerances of around +-10% of the original RC oscillator can be compensated but for tuning
- this is normally enough.
+ The oscillator speed adapts to the serial speed of the communication partner (e.g. a USB or blueetooth to serial converter),
+ so that the ATmega matches the baud rate of its partner. Only tolerances of +-10% of the original RC oscillator can
+ be compensated but normally this is enough.
  
  Instructions to use:
  1. Adapt the baud rate of Serial.begin() in the sketch source code
- 2. Upload the sketch and let the sketch run for some 10 minutes under the temperature conditions it will be used in future.
+ 2. Upload the sketch and let the sketch run for some 10 minutes under the temperature and voltage conditions it will be used in future.
     For normal room temperature (20 to 25 °C) this is not so critical, but when you want to use it in a closed cabinet
     at temperatures >40 or <5 °C this ensures better calibration.
  3. Connect to your board using the Arduino serial monitor or any other terminal program
  4. In the serial monitor send the following characters (one by one with a pause of at least 5m in between, not as one long string):
-      'D' (hex 0x44 => 3*0 bit time at the beginning)
-      'B' (hex 0x42 => 2*0 bit time at the beginning).
-    Keep on sending as long as the OSCCAL values are changing. Normally 15 charaters are enough.
-    When the OSCCAL values stabilizes after sending some characters, the calibration is finished.
+      'D' (hex 0x44, bin 01000100 => 2*0 bit time + start bit at the beginning)
+      'B' (hex 0x42, bin 01000010 => 1*0 bit time + start bit at the beginning).
+    Keep on sending the characters as long as the OSCCAL values are changing in the same direction, normally 15 charaters are enough.
+    When the OSCCAL values stabilizes after sending some characters and jumps only back and forth by 1, the calibration is finished.
  5. Use the printed value of 'g_medianOSCCAL' to set '-DOSCCAL_VALUE=xxx' in the BlueController optiboot Makefile
     (replace xxx with the value of g_medianOSCCAL). This will permanently set the OSCCAL value of the boot loader
     and the value will also be active when the boot loader is exited and the main program runs
+    You have to use the special version of the optiboot from here:
+    https://code.google.com/r/michaeldreher42-bluecontroller/source/browse/#hg%2Fbluecontroller%2Fbootloaders%2Foptiboot    
 
  Sending the characters '+' and '-' you can manually change OSCCAL and watch the effect on g_tDiff.
+ 
+ The whole calibration runs in the ISR, the main loop only prints the current values of the calibration.
  
  Copyright (c) 2013 by Michael Dreher <michael@5dot1.de>
  This example code is in the public domain.
@@ -34,12 +42,12 @@
 // Pin 13 has an LED connected on most Arduino boards.
 const uint8_t led = LED_BUILTIN;
 
-#if 0 // use fixed values or calculate the at runtime
+#if 0 // use fixed values to calculate the bit clocks at compile time or calculate the bit clocks at runtime
 // these constants are for 115200 baud with a oscillator freq. of 8MHz
 #define SER_DBLSPEED (1) // 0 or 1
 #define SER_UBRR (8)
 #else
-// dynamically calculate the values, the code will be ~1300 byte larger because it uses 32 bit calculations.
+// dynamically calculate the values from UBRR and U2X0, the code will be betwenn 500 and 1300 byte larger because it uses 32 bit calculations.
 #define SER_DBLSPEED ((UCSR0A >> U2X0) & 0x01) // 0 or 1
 #define SER_UBRR (UBRR0)
 #endif
@@ -47,8 +55,7 @@ const uint8_t led = LED_BUILTIN;
 // this calculates the real baudrate (incl. error), not the baud rate which is used as parameter for Serial.begin()
 #define SER_MUL ((2 - (SER_DBLSPEED ? 1 : 0)) * 8)
 #define SERIALBAUDRATE (F_CPU / (SER_MUL * (SER_UBRR + 1)))
-//#define SER_BIT_TIME(bits) ((((2 * bits * F_CPU) / SERIALBAUDRATE) + 1) / 2) // round up
-#define SER_BIT_TIME(bits) (bits * (SER_MUL * (SER_UBRR + 1))) // round up
+#define SER_BIT_TIME(bits) (bits * (SER_MUL * (SER_UBRR + 1)))
 
 // the tolarance used for comparing bit times (+-1/10=+-10%)
 const uint8_t tolerance = 1;
@@ -88,54 +95,19 @@ int16_t g_BitTime3Max;
 
 #define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit)) // clear bit 
 #define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))  // set bit
-#define __CAT2(base, portname) base##portname // internally needed by CAT2
-#define CAT2(prefix, num) __CAT2(prefix, num) // build a define name from 2 params
-#define __CAT3(prefix, num, postfix) prefix##num##postfix // internally needed by CAT3
-#define CAT3(prefix, num, postfix) __CAT3(prefix, num, postfix) // build a define name from 3 params
 
 // the setup routine runs once when you press reset:
 void setup() {
-  // ARDUINO_SERIAL_BAUDRATE is either defined in .../hardware/<boardname>/variants/<variant>/pins_arduino.h
-  // or you have to set it manually here when your board has no fixed baud rate
+  // ARDUINO_SERIAL_BAUDRATE is either defined in .../Arduino/hardware/<boardname>/variants/<variant>/pins_arduino.h
+  // or you have to set it manually here when your board has no fixed baud rate defined there
+  // currently only the BlueController boards define this constant
   Serial.begin(ARDUINO_SERIAL_BAUDRATE);
-  g_BitTime1 = SER_BIT_TIME(1);
-  g_BitTime1Min = (g_BitTime1 * (tolerancebase - tolerance)) / tolerancebase;     
-  g_BitTime1Max = (g_BitTime1 * (tolerancebase + tolerance)) / tolerancebase;     
-
-  g_BitTime2 = SER_BIT_TIME(2);
-  g_BitTime2Min = (g_BitTime2 * (tolerancebase - tolerance)) / tolerancebase;     
-  g_BitTime2Max = (g_BitTime2 * (tolerancebase + tolerance)) / tolerancebase;     
-
-  g_BitTime3 = SER_BIT_TIME(3);
-  g_BitTime3Min = (g_BitTime3 * (tolerancebase - tolerance)) / tolerancebase;     
-  g_BitTime3Max = (g_BitTime3 * (tolerancebase + tolerance)) / tolerancebase;     
-
-  // initialize the digital pin as an output.
   pinMode(led, OUTPUT);
 
-  g_OSCCAL_lbound = ((uint16_t)g_origOSCCAL * 9) / 10;
-  g_OSCCAL_ubound = ((uint16_t)g_origOSCCAL * 11) / 10;
-
-  if(g_OSCCAL_ubound <= g_origOSCCAL) // overflow
-    g_OSCCAL_ubound = 255;
-  
-  if(g_OSCCAL_lbound < 1L) // overflow
-    g_OSCCAL_lbound = 1;
-  
-  // stay in same segment as original OSCCAL, TODO: use both segments
-  if(g_origOSCCAL >= 128)
-  {
-    if(g_OSCCAL_lbound < 128)
-      g_OSCCAL_lbound = 128;
-  }
-  else
-  {
-    if(g_OSCCAL_ubound > 127)
-      g_OSCCAL_ubound = 127;
-  }
+  InitCalibration(); // must be called _after_ Serial.begin()
 
   TRACENAMEVAR("F_CPU", F_CPU);
-  TRACENAMEVAR("SERIALBAUDRATE", SERIALBAUDRATE);
+  TRACENAMEVAR("Nominal baud rate", SERIALBAUDRATE);
   TRACEVAR(g_BitTime1);
   TRACEVAR(g_BitTime1Min);
   TRACEVAR(g_BitTime1Max);
@@ -147,13 +119,10 @@ void setup() {
   TRACEVAR(g_BitTime3Max);
   TRACEVAR(g_OSCCAL_lbound);
   TRACEVAR(g_OSCCAL_ubound);
-
-  InitTimerPcintIrq();
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-#if 1
   static int16_t prevTdiff;
   if(g_tDiff != prevTdiff)
   {
@@ -161,7 +130,6 @@ void loop() {
     Serial.println(g_tDiff);
     prevTdiff = g_tDiff;
   }
-#endif
 
   static uint8_t prevOSCCAL;
   if(OSCCAL != prevOSCCAL)
@@ -170,19 +138,19 @@ void loop() {
     prevOSCCAL = OSCCAL;
   }
 
-  //bool newLine = false;
   if(Serial.available())
-  //while(Serial.available())
   {
     unsigned char c = Serial.read();
     if(c=='r')
     {
       Serial.println("Software reset");
+      delay(20);
       soft_reset();
     }
     else if(c=='b')
     {
       Serial.println("Entering boot loader");
+      delay(20);
       enter_bootloader();
     }
     else if(c=='+' && OSCCAL < 255)
@@ -214,9 +182,9 @@ void loop() {
 
 void printOSCCAL(void)
 {
-  // these two values are used for checking the baud rate with an oscilloscope or logic analyzer
-  Serial.write(0x55);
-  Serial.write(0x00);
+  // these values are useful for checking the baud rate with an oscilloscope or logic analyzer
+  Serial.write(0x55); // logic levels on the TXD line: 10 10101010 1
+  Serial.write(0x00); // logic levels on the TXD line:  0 00000000 1
 
   Serial.print("  OSCCAL=");
   Serial.print(OSCCAL);
@@ -224,8 +192,7 @@ void printOSCCAL(void)
   Serial.println(g_medianOSCCAL);
 }
 
-
-void InitTimerPcintIrq(void)
+inline void InitTimerPcintIrq(void)
 {
   // enable pin change IRQ
   unsigned char sreg = SREG;
@@ -236,13 +203,45 @@ void InitTimerPcintIrq(void)
   TCNT1 = 0;
   OCR1A = TCNT1 - 1;
   TIFR1 = _BV(OCF1A); 
-#if USE_OVL_ISR
-  TIMSK1 = _BV(OCIE1A); // enable output compare match IRQ
-#endif
 
   TCCR1A = 0x00;
   TCCR1B = _BV(CS10);
   SREG = sreg;
+}
+
+inline void InitCalibration(void)
+{
+  g_BitTime1 = SER_BIT_TIME(1);
+  g_BitTime1Min = (g_BitTime1 * (tolerancebase - tolerance)) / tolerancebase;     
+  g_BitTime1Max = (g_BitTime1 * (tolerancebase + tolerance)) / tolerancebase;     
+
+  g_BitTime2 = SER_BIT_TIME(2);
+  g_BitTime2Min = (g_BitTime2 * (tolerancebase - tolerance)) / tolerancebase;     
+  g_BitTime2Max = (g_BitTime2 * (tolerancebase + tolerance)) / tolerancebase;     
+
+  g_BitTime3 = SER_BIT_TIME(3);
+  g_BitTime3Min = (g_BitTime3 * (tolerancebase - tolerance)) / tolerancebase;     
+  g_BitTime3Max = (g_BitTime3 * (tolerancebase + tolerance)) / tolerancebase;     
+
+  g_OSCCAL_lbound = ((uint16_t)g_origOSCCAL * 9) / 10;
+  g_OSCCAL_ubound = ((uint16_t)g_origOSCCAL * 11) / 10;
+
+  if(g_OSCCAL_ubound <= g_origOSCCAL) // overflow
+    g_OSCCAL_ubound = 255;
+  
+  // stay in same segment as original OSCCAL, TODO: use both segments
+  if(g_origOSCCAL >= 128)
+  {
+    if(g_OSCCAL_lbound < 128)
+      g_OSCCAL_lbound = 128;
+  }
+  else
+  {
+    if(g_OSCCAL_ubound > 127)
+      g_OSCCAL_ubound = 127;
+  }
+
+  InitTimerPcintIrq();
 }
 
 inline void SetOSCCAL(int8_t offset)
@@ -325,14 +324,8 @@ ISR(PCINT2_vect)
   PCIFR = _BV(PCIF2);
 }
 
-
-#if USE_OVL_ISR
-ISR(TIMER1_COMPA_vect)
-{
-  g_timerOvl = true;
-}
-#endif
-
+//============================================================================
+// reset and enter bootloader functions
 #define ENTER_BL_MAGIC_VAL (0xc49e);
 #define ENTER_BL_MAGIC_ADR (*((volatile uint16_t*)0x100))
 void enter_bootloader(void)
